9c3baa39bbf8fd3e4b6cb3ece8a7accc4ceb0527
BenAwuor2 <ben.oketch2@gmail.com>
2022-06-20 08:31:47 -0700

    func

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    while True:
+    i = 0
+    while i < x:
         try:
-            if index < x:
-                print(my_list[index], end='')
-                index += 1
-            else:
-                print()
-                return index
+            print("{}".format(my_list[i]), end='')
         except IndexError:
-            print()
-            return 
+            break
+        i += 1
+    print("")
+    return i 
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-        print("Inside result: {:.1f}".format(result))
-    except:
-        result = None
-        print("Inside result: {}".format(result))
+        print("Inside result: ", end='')
+        total = a / b
+    except ZeroDivisionError:
+        total = None
     finally:
-        return result
+        print("{}".format(total))
+    return total
