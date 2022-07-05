18fa368343e1ae9fb061d23aedd250a87fbfa81a
BenAwuor2 <ben.oketch2@gmail.com>
2022-07-05 08:05:30 -0700

    new

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-''' function that returns the number of lines of a text file
-'''
+"""
+1-number_of_lines.py
+number_of_lines(filename="")
+"""
 
 
 def number_of_lines(filename=""):
-    ''' function: number_of_lines
-    '''
-    if filename == "" or type(filename) != str:
-        return 0
-    counter = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            counter += 1
-    return counter
+    """
+    Returns the number of lines of a text file
+    """
+    count = 0
+    with open(filename) as fil:
+        for line in fil:
+            count += 1
+    return count
