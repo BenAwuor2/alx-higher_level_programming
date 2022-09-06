918a0548b15d52307ba9541e5561fa5b65599bd0
BenAwuor2 <ben.oketch2@gmail.com>
2022-09-06 08:36:56 -0700

    l

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-let num = process.argv[2];
-
-if (isNaN(num)) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < num; i++) {
-    let msg = '';
-    for (let j = 0; j < num; j++) {
-      msg = msg + 'X';
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let square = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      square = square + 'X';
     }
-    console.log(msg);
+    console.log(square);
   }
 }
