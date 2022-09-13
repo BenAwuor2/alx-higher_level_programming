e361069a07449299d5f1c1972c0c1b869533b944
BenAwuor2 <ben.oketch2@gmail.com>
2022-09-13 06:40:23 -0700

    f

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="127.0.0.1",
         port=3306,
-        user=sql_user,
+        user=papaoketch2,
         passwd=P12a1995,
         db=hbtn_0e_0_usa
         )
