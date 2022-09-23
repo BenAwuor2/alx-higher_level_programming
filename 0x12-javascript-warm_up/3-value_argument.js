3bd331669df3eadafd4d1df2439621bb2b50a9a1
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2022-09-23 22:16:53 +0300

    python-network

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let myVar = process.argv.split(2);
+console.log(myVar);
+if{myVar === true;}
+else{
+    console.log("No argument");
+}
+
+/*if (process.argv[2] === undefined){
+    console.log("No argument");}
+else{console.log(process.argv[2]);}*/
