9e02d1f0fcb306962db0ffd08cc833488f1dbbb2
BenAwuor2 <ben.oketch2@gmail.com>
2022-09-23 13:16:35 -0700

    searchAPI

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 """
-Task 8:
-Sends POST request to http://0.0.0.0:5000/search_user
-8-json_api.py
+Script that takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
 """
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    q = ""
-    if len(argv) > 1:
-        q = argv[1]
-    req = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
-    try:
-        if req.json():
-            print("[{}] {}".format(req.json()["id"], req.json()["name"]))
-        elif len(req.json()) is 0:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
